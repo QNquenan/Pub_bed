df147caef61937a80b0a7a31426ddc33f2b97533
鹊楠吖~ <113289519+QNquenan@users.noreply.github.com>
2024-01-27 18:19:53 +0800

    Update runTime.js

--- js/runTime.js
+++ js/runTime.js
@@ -1,1 +1,33 @@
-var now = new Date; function createtime() { now.setTime(now.getTime() + 1e3); var e = new Date("08/01/2022 00:00:00"), t = Math.trunc(234e8 + (now - e) / 1e3 * 17), a = (t / 1496e5).toFixed(6), o = new Date("08/09/2022 00:00:00"), n = (now - o) / 1e3 / 60 / 60 / 24, r = Math.floor(n), i = (now - o) / 1e3 / 60 / 60 - 24 * r, s = Math.floor(i); 1 == String(s).length && (s = "0" + s); var d = (now - o) / 1e3 / 60 - 1440 * r - 60 * s, l = Math.floor(d); 1 == String(l).length && (l = "0" + l); var g = (now - o) / 1e3 - 86400 * r - 3600 * s - 60 * l, b = Math.round(g); 1 == String(b).length && (b = "0" + b); let c = ""; c = s < 18 && s >= 9 ? `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>` : `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>`, document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = c) } setInterval((() => { createtime() }), 1e3);
+var now = new Date;
+
+function createtime() {
+    // 更新当前时间
+    now.setTime(now.getTime() + 1e3);
+
+    // 计算时间间隔
+    var startDate = new Date("08/01/2022 00:00:00");
+    var travelDistance = Math.trunc(234e8 + (now - startDate) / 1e3 * 17);
+    var astronomicalUnits = (travelDistance / 1496e5).toFixed(6);
+
+    var endDate = new Date("08/09/2022 00:00:00");
+    var days = Math.floor((now - endDate) / 1e3 / 60 / 60 / 24);
+    var hours = Math.floor((now - endDate) / 1e3 / 60 / 60 - 24 * days);
+    var minutes = Math.floor((now - endDate) / 1e3 / 60 - 1440 * days - 60 * hours);
+    var seconds = Math.round((now - endDate) / 1e3 - 86400 * days - 3600 * hours - 60 * minutes);
+
+    // 根据条件生成 HTML 内容
+    var htmlContent = "";
+    htmlContent = (hours < 18 && hours >= 9) ?
+        `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${days} 天 ${hours} 小时 ${minutes} 分 ${seconds} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${travelDistance} 千米，约为 ${astronomicalUnits} 个天文单位 🚀</div>` :
+        `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${days} 天 ${hours} 小时 ${minutes} 分 ${seconds} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${travelDistance} 千米，约为 ${astronomicalUnits} 个天文单位 🚀</div>`;
+
+    // 更新页面内容
+    if (document.getElementById("workboard")) {
+        document.getElementById("workboard").innerHTML = htmlContent;
+    }
+}
+
+// 每隔一秒调用一次 createtime 函数
+setInterval(() => {
+    createtime();
+}, 1000);
